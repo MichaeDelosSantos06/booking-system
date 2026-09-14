b8cac71adc207432974467e3407b5015eb2ddc28
Michael <delossantosmichael09@gmail.com>
2026-09-14 15:27:42 +0800

    disabling seed for deployment-seed already migrated on the neon db

--- backend/src/lib/seed.ts
+++ backend/src/lib/seed.ts
@@ -1,36 +1,36 @@
-import bcrypt from "bcrypt";
-import { env } from "../config/env.js";
-import prisma from "./prisma.js";
+// import bcrypt from "bcrypt";
+// import { env } from "../config/env.js";
+// import prisma from "./prisma.js";
 
-async function main() {
-  const email = env.SEED_ADMIN_USER!;
-  const password = env.SEED_ADMIN_PASS!;
+// async function main() {
+//   const email = env.SEED_ADMIN_USER!;
+//   const password = env.SEED_ADMIN_PASS!;
 
-  const hashedPassword = await bcrypt.hash(password, 12);
+//   const hashedPassword = await bcrypt.hash(password, 12);
 
-  await prisma.user.upsert({
-    where: {
-      email,
-    },
-    update: {},
-    create: {
-      name: "Admin Ky",
-      email,
-      passwordHash: hashedPassword,
-      contact: "09917279195",
-      role: "Admin",
-      status: "Active",
-    },
-  });
+//   await prisma.user.upsert({
+//     where: {
+//       email,
+//     },
+//     update: {},
+//     create: {
+//       name: "Admin Ky",
+//       email,
+//       passwordHash: hashedPassword,
+//       contact: "09917279195",
+//       role: "Admin",
+//       status: "Active",
+//     },
+//   });
 
-  console.log("Admin account seeded successfully.");
-}
+//   console.log("Admin account seeded successfully.");
+// }
 
-main()
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+// main()
+//   .catch((error) => {
+//     console.error(error);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
